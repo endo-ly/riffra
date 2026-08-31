4641c3ee1f193629e82e79b01da510dd75efd0e1
ryuto.endo <ryuto9007@gmail.com>
2026-08-31 03:06:32 +0000

    test(core): pin harmony acceptance corpus

--- crates/riffra-core/src/domain/music/harmony.rs
+++ crates/riffra-core/src/domain/music/harmony.rs
@@ -172,30 +172,438 @@ mod tests {
     use super::*;
 
     #[test]
-    fn resolves_common_and_enharmonic_chords_with_canonical_spelling() {
+    fn resolves_the_riffra_chord_acceptance_corpus() {
+        struct AcceptedChord {
+            symbol: &'static str,
+            name: &'static str,
+            root: &'static str,
+            bass: Option<&'static str>,
+            tones: &'static [&'static str],
+        }
+
         let cases = [
-            ("Dm9", "Dm9", "D", None, vec!["D", "F", "A", "C", "E"]),
-            (
-                "G7(b9,#11)/F",
-                "G7b9#11/F",
-                "G",
-                Some("F"),
-                vec!["G", "B", "D", "F", "Ab", "C#"],
-            ),
-            ("Cdim7", "Cdim7", "C", None, vec!["C", "Eb", "Gb", "Bbb"]),
-            ("Cm7b5", "Cm7b5", "C", None, vec!["C", "Eb", "Gb", "Bb"]),
-            ("Cø", "Cm7b5", "C", None, vec!["C", "Eb", "Gb", "Bb"]),
-            ("Cmaj9", "Cmaj9", "C", None, vec!["C", "E", "G", "B", "D"]),
-            ("Abaug/Bb", "Ab+/Bb", "Ab", Some("Bb"), vec!["Ab", "C", "E"]),
+            AcceptedChord {
+                symbol: "C",
+                name: "C",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G"],
+            },
+            AcceptedChord {
+                symbol: "Cm",
+                name: "Cm",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "G"],
+            },
+            AcceptedChord {
+                symbol: "C5",
+                name: "C5",
+                root: "C",
+                bass: None,
+                tones: &["C", "G"],
+            },
+            AcceptedChord {
+                symbol: "Csus2",
+                name: "Cadd9omt3",
+                root: "C",
+                bass: None,
+                tones: &["C", "D", "G"],
+            },
+            AcceptedChord {
+                symbol: "Csus4",
+                name: "Csus",
+                root: "C",
+                bass: None,
+                tones: &["C", "F", "G"],
+            },
+            AcceptedChord {
+                symbol: "Caug",
+                name: "C+",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G#"],
+            },
+            AcceptedChord {
+                symbol: "C+",
+                name: "C+",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G#"],
+            },
+            AcceptedChord {
+                symbol: "Cdim",
+                name: "Cdim",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "Gb"],
+            },
+            AcceptedChord {
+                symbol: "C°",
+                name: "Cdim",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "Gb"],
+            },
+            AcceptedChord {
+                symbol: "C6",
+                name: "C6",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "A"],
+            },
+            AcceptedChord {
+                symbol: "Cm6",
+                name: "Cm6",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "G", "A"],
+            },
+            AcceptedChord {
+                symbol: "Cmaj7",
+                name: "Cmaj7",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "B"],
+            },
+            AcceptedChord {
+                symbol: "C7",
+                name: "C7",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "Bb"],
+            },
+            AcceptedChord {
+                symbol: "Cm7",
+                name: "Cm7",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "G", "Bb"],
+            },
+            AcceptedChord {
+                symbol: "CmMaj7",
+                name: "Cmmaj7",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "G", "B"],
+            },
+            AcceptedChord {
+                symbol: "Cdim7",
+                name: "Cdim7",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "Gb", "Bbb"],
+            },
+            AcceptedChord {
+                symbol: "C°7",
+                name: "Cdim7",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "Gb", "Bbb"],
+            },
+            AcceptedChord {
+                symbol: "Cm7b5",
+                name: "Cm7b5",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "Gb", "Bb"],
+            },
+            AcceptedChord {
+                symbol: "Cø7",
+                name: "Cm7b5",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "Gb", "Bb"],
+            },
+            AcceptedChord {
+                symbol: "C9",
+                name: "C9",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "Bb", "D"],
+            },
+            AcceptedChord {
+                symbol: "Cmaj9",
+                name: "Cmaj9",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "B", "D"],
+            },
+            AcceptedChord {
+                symbol: "Cm9",
+                name: "Cm9",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "G", "Bb", "D"],
+            },
+            AcceptedChord {
+                symbol: "C11",
+                name: "C9sus",
+                root: "C",
+                bass: None,
+                tones: &["C", "F", "G", "Bb", "D"],
+            },
+            AcceptedChord {
+                symbol: "Cmaj11",
+                name: "Cmaj9sus",
+                root: "C",
+                bass: None,
+                tones: &["C", "F", "G", "B", "D"],
+            },
+            AcceptedChord {
+                symbol: "Cm11",
+                name: "Cm11",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "G", "Bb", "D", "F"],
+            },
+            AcceptedChord {
+                symbol: "C13",
+                name: "C13",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "Bb", "D", "A"],
+            },
+            AcceptedChord {
+                symbol: "Cmaj13",
+                name: "C69addmaj7",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "B", "D", "A"],
+            },
+            AcceptedChord {
+                symbol: "Cm13",
+                name: "Cm13",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "G", "Bb", "D", "F", "A"],
+            },
+            AcceptedChord {
+                symbol: "Cadd9",
+                name: "Cadd9",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "D"],
+            },
+            AcceptedChord {
+                symbol: "Cadd11",
+                name: "Csusadd3",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "F", "G"],
+            },
+            AcceptedChord {
+                symbol: "C7omit5",
+                name: "C7omt5",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "Bb"],
+            },
+            AcceptedChord {
+                symbol: "C7b5",
+                name: "C7b5",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "Gb", "Bb"],
+            },
+            AcceptedChord {
+                symbol: "C7#5",
+                name: "C7#5",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G#", "Bb"],
+            },
+            AcceptedChord {
+                symbol: "C7b9",
+                name: "C7b9",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "Bb", "Db"],
+            },
+            AcceptedChord {
+                symbol: "C7#9",
+                name: "C7#9",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "Bb", "D#"],
+            },
+            AcceptedChord {
+                symbol: "C7#11",
+                name: "C7#11",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "Bb", "F#"],
+            },
+            AcceptedChord {
+                symbol: "C7b13",
+                name: "C7b13",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "Bb", "Ab"],
+            },
+            AcceptedChord {
+                symbol: "C7(b9,#11)",
+                name: "C7b9#11",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "Bb", "Db", "F#"],
+            },
+            AcceptedChord {
+                symbol: "G7(b9,#9,b13)",
+                name: "G7b9#9b13",
+                root: "G",
+                bass: None,
+                tones: &["G", "B", "F", "Ab", "A#", "Eb"],
+            },
+            AcceptedChord {
+                symbol: "C/E",
+                name: "C/E",
+                root: "C",
+                bass: Some("E"),
+                tones: &["C", "E", "G"],
+            },
+            AcceptedChord {
+                symbol: "C/Bb",
+                name: "C/Bb",
+                root: "C",
+                bass: Some("Bb"),
+                tones: &["C", "E", "G"],
+            },
+            AcceptedChord {
+                symbol: "Abaug/Bb",
+                name: "Ab+/Bb",
+                root: "Ab",
+                bass: Some("Bb"),
+                tones: &["Ab", "C", "E"],
+            },
+            AcceptedChord {
+                symbol: "G7(b9,#11)/F",
+                name: "G7b9#11/F",
+                root: "G",
+                bass: Some("F"),
+                tones: &["G", "B", "D", "F", "Ab", "C#"],
+            },
+            AcceptedChord {
+                symbol: "F#m7",
+                name: "F#m7",
+                root: "F#",
+                bass: None,
+                tones: &["F#", "A", "C#", "E"],
+            },
+            AcceptedChord {
+                symbol: "Gbmaj7",
+                name: "Gbmaj7",
+                root: "Gb",
+                bass: None,
+                tones: &["Gb", "Bb", "Db", "F"],
+            },
+            AcceptedChord {
+                symbol: "Bb7",
+                name: "Bb7",
+                root: "Bb",
+                bass: None,
+                tones: &["Bb", "D", "F", "Ab"],
+            },
+            AcceptedChord {
+                symbol: "Ebmaj9",
+                name: "Ebmaj9",
+                root: "Eb",
+                bass: None,
+                tones: &["Eb", "G", "Bb", "D", "F"],
+            },
+            AcceptedChord {
+                symbol: "Cbmaj7",
+                name: "Cbmaj7",
+                root: "Cb",
+                bass: None,
+                tones: &["Cb", "Eb", "Gb", "Bb"],
+            },
+            AcceptedChord {
+                symbol: "C#minomit5maj7add9#11",
+                name: "C#dimaddmaj79omt5",
+                root: "C#",
+                bass: None,
+                tones: &["C#", "E", "B#", "D#", "F##"],
+            },
+            AcceptedChord {
+                symbol: "CM",
+                name: "C",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G"],
+            },
+            AcceptedChord {
+                symbol: "Cmaj",
+                name: "C",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G"],
+            },
+            AcceptedChord {
+                symbol: "CΔ",
+                name: "Cmaj7",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G", "B"],
+            },
+            AcceptedChord {
+                symbol: "Cmin",
+                name: "Cm",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "G"],
+            },
+            AcceptedChord {
+                symbol: "C-",
+                name: "Cm",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "G"],
+            },
+            AcceptedChord {
+                symbol: "Cdim",
+                name: "Cdim",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "Gb"],
+            },
+            AcceptedChord {
+                symbol: "C°",
+                name: "Cdim",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "Gb"],
+            },
+            AcceptedChord {
+                symbol: "Cø",
+                name: "Cm7b5",
+                root: "C",
+                bass: None,
+                tones: &["C", "Eb", "Gb", "Bb"],
+            },
+            AcceptedChord {
+                symbol: "Caug",
+                name: "C+",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G#"],
+            },
+            AcceptedChord {
+                symbol: "C+",
+                name: "C+",
+                root: "C",
+                bass: None,
+                tones: &["C", "E", "G#"],
+            },
         ];
 
-        for (symbol, name, root, bass, tones) in cases {
-            let chord = HarmonyChord::resolve(symbol).unwrap();
-            assert_eq!(chord.name, name);
-            assert_eq!(chord.root.unwrap().to_string(), root);
+        for case in cases {
+            let chord = HarmonyChord::resolve(case.symbol).unwrap();
+            assert_eq!(chord.name, case.name);
+            assert_eq!(chord.root.unwrap().to_string(), case.root);
             assert_eq!(
                 chord.bass.map(|note| note.to_string()),
-                bass.map(str::to_owned)
+                case.bass.map(str::to_owned)
             );
             assert_eq!(
                 chord
@@ -203,19 +611,9 @@ mod tests {
                     .iter()
                     .map(ToString::to_string)
                     .collect::<Vec<_>>(),
-                tones
+                case.tones
             );
         }
-
-        let enharmonic = HarmonyChord::resolve("C#minomit5maj7add9#11").unwrap();
-        assert_eq!(
-            enharmonic
-                .tones
-                .iter()
-                .map(ToString::to_string)
-                .collect::<Vec<_>>(),
-            ["C#", "E", "B#", "D#", "F##"]
-        );
     }
 
     #[test]
